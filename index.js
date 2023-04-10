e036862d81e6e6ce7468e0434b0ceafd553db81d
Mohammed Akbar Alli <hafizakbar19@gmail.com>
2023-04-10 13:54:16 +0500

    .

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#! usr/bin/env node
 import { add, multiply, sub, div } from "./add.js";
 import inquirer from "inquirer";
 const answers = await inquirer.prompt([
@@ -33,6 +34,3 @@ if (answers.sign === "/") {
     const ans = div(answers.num1, answers.num2);
     console.log(ans);
 }
-// console.log("hello");// syncronus and asyncronus video of hamza  //npm login //shibang 
-// #1 usr/bin/env node  write this command in the start of the program | npm installs and npx can execute directly from the cloud.
-//commad to use is npx calculator | 
